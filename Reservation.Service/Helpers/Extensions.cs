e2267eeed254f206b5c2548772643ae1ec88ec2c
Ben Atanesyan <benatanesyan2@gmail.com>
2022-02-26 00:00:10 +0400

    removed unnecessary comment

--- Reservation.Service/Helpers/Extensions.cs
+++ Reservation.Service/Helpers/Extensions.cs
@@ -13,7 +13,6 @@ namespace Reservation.Service.Helpers
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                //From String to byte array
                 byte[] sourceBytes = Encoding.UTF8.GetBytes(password);
                 byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
                 string hashedPassword = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
