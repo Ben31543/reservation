0bc98f38d16a033b530e0cfcb721219b4151dba4
Ben Atanesyan <benatanesyan2@gmail.com>
2022-05-03 01:02:14 +0400

    removed is attached flag from bank card entity

---
+++ Reservation.Data/Migrations/20220502210012_RemovedIsAttachedFlagFromBankCard.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reservation.Data.Migrations
+{
+    public partial class RemovedIsAttachedFlagFromBankCard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAttached",
+                table: "BankCards");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAttached",
+                table: "BankCards",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
